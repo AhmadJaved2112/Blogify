0b803d9c4bebe7c0c121a608cc234c2730fa970c
Ahmad Javed 2112 <ahmadjaved2112sgc@gmail.com>
2026-01-30 13:40:14 +0500

    Enhance Blogs page with header and description
    Added a header section with title and description to the Blogs page.

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -5,9 +5,19 @@ import Pagination from '../components/Pagination'
 const Blogs = () => {
   return (
     <div>
+      <div className="text-center mb-12 bg-linear-to-r from-gray-900 to-black py-20 md:py-30">
+        <h1 className="text-4xl md:text-5xl font-bold text-white mb-4">
+          Our <span className="text-orange-500">Blog</span> & Insights
+        </h1>
+        <p className="text-lg text-gray-300 max-w-3xl mx-auto">
+          Discover the latest trends, expert tips, and industry insights to help you stay ahead in the digital world.
+        </p>
+      </div>
+
       <div className="container mx-auto px-4 py-3">
         <CategoryBtns />
         <BlogsLayout />
+
         <Pagination />
       </div>
     </div>
